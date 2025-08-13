7c42720d586c70f6a22b15f3dbaab5604796ac1b
Kenn <kennethbtst25@gmail.com>
2025-08-13 19:59:03 +0800

    adding-updater-order-status

--- routes/Order.routes.js
+++ routes/Order.routes.js
@@ -3,10 +3,13 @@ const OrderRouter = new express.Router();
 const {
   AddOrder,
   GetAllOrderPerCompany,
+  UpdateOrderStatus,
 } = require("../controllers/Order.controller");
 
 OrderRouter.post("/api/orders", AddOrder);
 
 OrderRouter.get("/api/orders", GetAllOrderPerCompany);
+
+OrderRouter.put("/api/orders/:id/status", UpdateOrderStatus);
 
 module.exports = OrderRouter;
