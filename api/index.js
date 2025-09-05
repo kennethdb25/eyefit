f26d47d979d763cc3e8ad1652c2739f29e37411e
Kenn <kennethbtst25@gmail.com>
2025-09-05 20:37:26 +0800

    fix

--- api/index.js
+++ api/index.js
@@ -33,10 +33,10 @@ app.use(InventoryRouter);
 module.exports = app;
 // end
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
+//   app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//   });
+// }
