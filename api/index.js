b5b0a7df1d5686490b9c2717aaaaaa4457a69b57
Kenn <kennethbtst25@gmail.com>
2025-09-05 20:32:40 +0800

    for-versel

--- api/index.js
+++ api/index.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-require("../config/database/db.conf");
+require("./config/database/db.conf");
 require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // const uploadRoutes = require("./routes/upload");
-const AccountRouter = require("../routes/Account.routes");
-const ProductRouter = require("../routes/Product.routes");
-const AppointmentRouter = require("../routes/Appointment.routes");
-const UserRouter = require("../routes/User.routes");
-const OrderRouter = require("../routes/Order.routes");
-const DeliveryRouter = require("../routes/Delivery.routes");
-const NotificationRouter = require("../routes/Notification.routes");
-const InventoryRouter = require("../routes/Inventory.routes");
+const AccountRouter = require("./routes/Account.routes");
+const ProductRouter = require("./routes/Product.routes");
+const AppointmentRouter = require("./routes/Appointment.routes");
+const UserRouter = require("./routes/User.routes");
+const OrderRouter = require("./routes/Order.routes");
+const DeliveryRouter = require("./routes/Delivery.routes");
+const NotificationRouter = require("./routes/Notification.routes");
+const InventoryRouter = require("./routes/Inventory.routes");
 
 // app.use("/api", uploadRoutes);
 app.use(AccountRouter);
@@ -30,9 +30,7 @@ app.use(InventoryRouter);
 // const PORT = process.env.PORT || 8080;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-
 module.exports = app;
-
 // end
 
 if (process.env.NODE_ENV === "production") {
