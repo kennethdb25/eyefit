4e013ae4fa3318be86d3ee82a195f649e2c8ba7e
Kenn <kennethbtst25@gmail.com>
2025-09-05 19:26:49 +0800

    adding-cloudinary-secrets

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,7 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  cloudName: process.env.CLOUDNAME,
+  apiKey: process.env.APIKEY,
+  apiSecret: process.env.APISECRET,
 };
