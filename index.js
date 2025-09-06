968c39b0fcd54d607a8921175651f14013982abe
Kenn <kennethbtst25@gmail.com>
2025-09-06 10:33:21 +0800

    fix

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-// const uploadRoutes = require("./routes/upload");
 const AccountRouter = require("./routes/Account.routes");
 const ProductRouter = require("./routes/Product.routes");
 const AppointmentRouter = require("./routes/Appointment.routes");
@@ -20,25 +19,30 @@ const DeliveryRouter = require("./routes/Delivery.routes");
 const NotificationRouter = require("./routes/Notification.routes");
 const InventoryRouter = require("./routes/Inventory.routes");
 
-// app.use("/api", uploadRoutes);
 app.use(AccountRouter);
+console.log('Loading AccountRouter');
 app.use(ProductRouter);
+console.log('Loading ProductRouter');
 app.use(AppointmentRouter);
+console.log('Loading AppointmentRouter');
 app.use(UserRouter);
+console.log('Loading UserRouter');
 app.use(OrderRouter);
+console.log('Loading OrderRouter');
 app.use(DeliveryRouter);
+console.log('Loading DeliveryRouter');
 app.use(NotificationRouter);
+console.log('Loading NotificationRouter');
 app.use(InventoryRouter);
+console.log('Loading InventoryRouter');
 
-// module.exports = app;
-// end
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", function (req, res) {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
