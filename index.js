881ab933bee2195caf7cf3563a591d33be7cb668
Kenn <kennethbtst25@gmail.com>
2025-09-06 11:05:25 +0800

    fix

--- index.js
+++ index.js
@@ -40,12 +40,12 @@ app.use(InventoryRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
+//   app.get("/*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//   });
+// }
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
